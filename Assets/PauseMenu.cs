ff5ee85bc318a5c27afbc65e9edfb5fe403242b5
Farrel Zaindri <dracko524@gmail.com>
2026-06-08 11:12:21 +0700

    MainMenu dan PauseMenu v1

---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] GameObject pauseMenu;
+    public void Pause()
+    {
+        pauseMenu.SetActive(true);
+    }
+
+    public void Home()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+    }
+}
